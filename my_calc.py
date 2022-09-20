98243ebdb85303bd696b4e20b7dc0a2ab7d718c9
Evgeny Shamray <EvSha83@yandex.ru>
2022-09-20 20:17:41 +0300

    "change file my_calc and add file for test my_calc "

--- my_calc.py
+++ my_calc.py
@@ -1,17 +1,27 @@
 def sum(a, b):
+    if not all(map(lambda x: isinstance(x,(int, float)),(a,b))):
+        raise TypeError("Аргументы a и b должны быть целыми или вещественными числами")
     return a + b
 
 
 def minus(a, b):
+    if not all(map(lambda x: isinstance(x,(int, float)),(a,b))):
+        raise TypeError("Аргументы a и b должны быть целыми или вещественными числами")
     return a - b
 
 
 def multi(a, b):
+    if not all(map(lambda x: isinstance(x,(int, float)),(a,b))):
+        raise TypeError("Аргументы a и b должны быть целыми или вещественными числами")
     return a * b
 
 
 def division(a, b):
+    if not all(map(lambda x: isinstance(x,(int, float)),(a,b))):
+        raise TypeError("Аргументы a и b должны быть целыми или вещественными числами")
     try:
         return a / b
     except ZeroDivisionError:
-        return "На ноль делить нельзя"
+        raise ValueError('Делить число на 0 нельзя')
+
+
